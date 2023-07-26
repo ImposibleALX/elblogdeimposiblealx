94aecb835af327147b6dc71f4f1c03e7432acc91
ImposibleALX <135774138+ImposibleALX@users.noreply.github.com>
2023-07-26 09:17:33 -0500

    Update dvd-logo.js

--- dvd-logo.js
+++ dvd-logo.js
@@ -4,53 +4,38 @@ const ctx = canvas.getContext('2d');
 const logo = new Image();
 logo.src = 'paralelo.png'; // Replace 'paralelo.png' with the actual path to your PNG image
 
-const maxCanvasWidth = 10; // Adjust this value as needed
-const maxCanvasHeight = 10; // Adjust this value as needed
+const logoWidth = 100; // Adjust this value as needed
+const logoHeight = 60; // Adjust this value as needed
 
-let logoWidth;
-let logoHeight;
-let x;
-let y;
-let dx = 1; // Horizontal speed
-let dy = 1; // Vertical speed
+const canvasWidth = window.innerWidth;
+const canvasHeight = window.innerHeight;
+
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+
+let x = Math.random() * (canvasWidth - logoWidth);
+let y = Math.random() * (canvasHeight - logoHeight);
+let dx = 2; // Horizontal speed
+let dy = 2; // Vertical speed
 
 function animate() {
   requestAnimationFrame(animate);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
   ctx.drawImage(logo, x, y, logoWidth, logoHeight);
 
   x += dx;
   y += dy;
 
-  if (x < 0 || x + logoWidth > canvas.width) {
+  if (x < 0 || x + logoWidth > canvasWidth) {
     dx = -dx;
     x += dx * 2;
   }
 
-  if (y < 0 || y + logoHeight > canvas.height) {
+  if (y < 0 || y + logoHeight > canvasHeight) {
     dy = -dy;
     y += dy * 2;
   }
 }
 
-logo.onload = function () {
-  const aspectRatio = logo.width / logo.height;
-
-  // Set canvas size to match the image size, considering the maximum canvas size
-  if (aspectRatio > 1) {
-    logoWidth = Math.min(logo.width, maxCanvasWidth);
-    logoHeight = logoWidth / aspectRatio;
-  } else {
-    logoHeight = Math.min(logo.height, maxCanvasHeight);
-    logoWidth = logoHeight * aspectRatio;
-  }
-
-  canvas.width = logoWidth;
-  canvas.height = logoHeight;
-
-  x = Math.random() * (canvas.width - logoWidth);
-  y = Math.random() * (canvas.height - logoHeight);
-
-  animate();
-};
+logo.onload = animate;
