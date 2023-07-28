e8df5f7128e3ca608bcf086483d9c90a06c5baa1
ImposibleALX <135774138+ImposibleALX@users.noreply.github.com>
2023-07-27 20:41:23 -0500

    Update barra.py

--- barra.py
+++ barra.py
@@ -2,8 +2,9 @@ import time
 BAR_LEN = 24
 elements = ['-', '\\', '|', '/']
 
-for i in range (BAR_LEN+1):
-  frame = i % len(elements)
-  print(f'\r[{elements[frame]*i:=<{BAR_LEN}}]', end='')
+while True:
+    for i in range (BAR_LEN+1):
+        frame = i % len(elements)
+        print(f'\r[{elements[frame]*i:=<{BAR_LEN}}]', end='')
 
-  time.sleep(0.2)
+        time.sleep(0.2)
